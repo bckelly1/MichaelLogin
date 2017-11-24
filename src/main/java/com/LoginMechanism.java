ffd1ea42d4721694747772c4e5f6680843624210
Brian Kelly <wolfyman06@gmail.com>
2017-11-24 14:07:01 -0700

    adding files

---
+++ src/main/java/com/LoginMechanism.java
@@ -0,0 +1,23 @@
+package com;
+
+public class LoginMechanism {
+    /**
+     * Requirements
+     *
+     * User can register
+     * user has ID
+     * User can "log in"
+     * User can have friends
+     * add/list all/remove
+     * blog - only writer can edit
+     *  List topics from a user
+     *  add topic for a user from a friend
+     *  remove a topic
+     * User can import/export
+     * reset all data
+     *
+     */
+
+
+
+}
